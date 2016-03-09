5e8a8bc21e200c6bfdc29ee1c233d99bd770313b
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-09 10:07:18 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
 from subprocess import call
 print("---------------------")
 print("ciao ciao")
-call(["docker","run --rm -it -p 9595:8888 -v "/$(pwd):/notebooks" docker-td"])
+call(["docker","run --rm -it -p 9595:8888 -v \"$(pwd):/notebooks\" docker-td"])
