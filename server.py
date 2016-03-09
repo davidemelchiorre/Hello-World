7bbd6e08145722db7113b09a376979d6fbdba9f7
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-09 15:57:37 +0100

    Update server.py

--- server.py
+++ server.py
@@ -6,10 +6,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
 conn, addr = s.accept()
-print ("Connected by", addr)
+print "Connected by", addr
 while 1:
     data = conn.recv(1024)
     if not data:break
-    print("Ricevuto:",data.decode('utf-8'))
+    print"Ricevuto:",data.decode('utf-8')
     conn.send(data)
 conn.close()
