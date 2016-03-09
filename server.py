e01aac724562f5086eccea0ba02ff27ba9105708
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-09 17:19:21 +0100

    Update server.py

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ client, buf = server.accept()
 while 1:
     buf = client.recv(buflen)
     if not buf:break
-    cmd=buf.decode('utf8')
+    cmd=buf[1]
     print "CMD=",cmd
     client.send("ack".encode('utf8'))
 client.close()
