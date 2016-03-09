07d33b5563f258ff03a32fafc0528b8d84cb653a
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-09 16:11:16 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,16 @@
 import socket
 
-addr = ''
 port = 9696
 buflen=1024
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((addr, port))
+server.bind((socket.gethostname(), port))
 server.listen(1)
-conn, buf = server.accept()
+client, buf = server.accept()
 while 1:
-    buf = conn.recv(buflen)
+    buf = client.recv(buflen)
     if not buf:break
     cmd=data.decode('utf8')
     print("CMD=",cmd)
-    conn.send("ack".encode('utf8'))
-conn.close()
+    client.send("ack".encode('utf8'))
+client.close()
