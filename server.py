048169dccfc6710c3cacef7a5f7425ab113cadc4
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-09 15:51:12 +0100

    Added files via upload

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+
+HOST = ''
+PORT = 9696
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print ("Connected by", addr)
+while 1:
+    data = conn.recv(1024)
+    if not data:break
+    print("Ricevuto:",data.decode('utf-8'))
+    conn.send(data)
+conn.close()
