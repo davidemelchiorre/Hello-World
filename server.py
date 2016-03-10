55d5cb210968db54fe621b5a949a35030273e44c
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-10 11:41:31 +0100

    Update server.py

--- server.py
+++ server.py
@@ -14,7 +14,23 @@ server.listen(1)
 print "D"
 while 1:
     print "In attesa di connessione..."
-    
-    print "E"
-
+    client, buf = server.accept()
+    #-------------------------
+    buf = client.recv(buflen)
+    if not buf:break
+    cmd=buf.decode('utf-8')
+    client.send(bytes(0))
+    #--------------------------
+    buf = client.recv(buflen)
+    if not buf:break
+    arg=buf.decode('utf-8')
+    client.send(bytes(0))
+    #--------------------------
+    print cmd," ",arg
+    if cmd=="close" and arg=="socket":
+        client.close()
+        server.close()
+    if cmd=="close" and arg=="client":
+        client.close()
+client.close()
 server.close()
