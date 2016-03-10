b701e86edb803bddb40cbbbc50a6ea8c6187a23b
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-10 11:33:20 +0100

    Update server.py

--- server.py
+++ server.py
@@ -3,9 +3,13 @@ import socket
 port = 9696
 buflen=1024
 
+print "A"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print "B"
 server.bind((socket.gethostname(), port))
+print "C"
 server.listen(1)
+print "D"
 while 1:
     print "In attesa di connessione..."
     client, buf = server.accept()
