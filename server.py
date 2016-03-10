f2024b0cde1fec7204651f177590d3d6ee8f8c88
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-10 11:38:22 +0100

    Update server.py

--- server.py
+++ server.py
@@ -7,5 +7,10 @@ buflen=1024
 
 print "A"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print "B"
+server.bind((socket.gethostname(), port))
+print "C"
+server.listen(1)
+print "D"
 
 server.close()
