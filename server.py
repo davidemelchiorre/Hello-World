0983ac35e8f6c14b2bb5edb51c475b36d5447b62
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-10 11:37:52 +0100

    Update server.py

--- server.py
+++ server.py
@@ -7,30 +7,5 @@ buflen=1024
 
 print "A"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print "B"
-server.bind((socket.gethostname(), port))
-print "C"
-server.listen(1)
-print "D"
-while 1:
-    print "In attesa di connessione..."
-    client, buf = server.accept()
-    #-------------------------
-    buf = client.recv(buflen)
-    if not buf:break
-    cmd=buf.decode('utf-8')
-    client.send(bytes(0))
-    #--------------------------
-    buf = client.recv(buflen)
-    if not buf:break
-    arg=buf.decode('utf-8')
-    client.send(bytes(0))
-    #--------------------------
-    print cmd," ",arg
-    if cmd=="close" and arg=="socket":
-        client.close()
-        server.close()
-    if cmd=="close" and arg=="client":
-        client.close()
-client.close()
+
 server.close()
