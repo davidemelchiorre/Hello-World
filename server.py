7e718d9c3520d92758715342f361a5839de28119
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-10 11:39:19 +0100

    Update server.py

--- server.py
+++ server.py
@@ -12,5 +12,9 @@ server.bind((socket.gethostname(), port))
 print "C"
 server.listen(1)
 print "D"
-
+while 1:
+    print "In attesa di connessione..."
+    client, buf = server.accept()
+    print "E"
+client.close()
 server.close()
