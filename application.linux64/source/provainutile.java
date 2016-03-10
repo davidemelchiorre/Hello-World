9812f0a787eb5dd218315e86f2758a6136f73712
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-10 11:54:14 +0100

    Added files via upload

---
+++ application.linux64/source/provainutile.java
@@ -0,0 +1,35 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class provainutile extends PApplet {
+
+public void setup()
+{
+  
+}
+public void draw()
+{
+  clear();
+  text("Hello world",width/2,height/2);
+}
+  public void settings() {  size(800,600); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "provainutile" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
---
+++ application.linux64/source/provainutile.pde
@@ -0,0 +1,9 @@
+void setup()
+{
+  size(800,600);
+}
+void draw()
+{
+  clear();
+  text("Hello world",width/2,height/2);
+}
