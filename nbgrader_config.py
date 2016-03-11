b0f80520a2fb78b2806426ea646733d8aceeef04
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-11 11:39:24 +0100

    Create nbgrader_config.py

---
+++ nbgrader_config.py
@@ -0,0 +1,7 @@
+c = get_config()
+c.NbGrader.db_assignments = [dict(name="001")]
+c.NbGrader.db_students = [
+    dict(id="bitdiddle", first_name="Ben", last_name="Bitdiddle"),
+    dict(id="hacker", first_name="Alyssa", last_name="Hacker"),
+    dict(id="reasoner", first_name="Louis", last_name="Reasoner")
+]
