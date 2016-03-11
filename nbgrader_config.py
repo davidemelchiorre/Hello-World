c7eab77580416af7fca0e8065b4b11ef2e72604d
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-11 11:53:39 +0100

    Delete nbgrader_config.py

--- nbgrader_config.py
+++
@@ -1,7 +0,0 @@
-c = get_config()
-c.NbGrader.db_assignments = [dict(name="001")]
-c.NbGrader.db_students = [
-    dict(id="bitdiddle", first_name="Ben", last_name="Bitdiddle"),
-    dict(id="hacker", first_name="Alyssa", last_name="Hacker"),
-    dict(id="reasoner", first_name="Louis", last_name="Reasoner")
-]
